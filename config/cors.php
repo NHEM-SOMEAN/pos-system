4abd7adc7ba7b9e696290e5f4fc71813c8b87f7d
NHEM-SOMEAN <nhemsomean@gmail.com>
2026-03-19 21:51:05 +0700

    fix: add Vercel URL to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+    'http://localhost:5173',
+    'https://pos-system-63z74m3rc-nhem-someans-projects.vercel.app',
+],
 
     'allowed_origins_patterns' => [],
 
